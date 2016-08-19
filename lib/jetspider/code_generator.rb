190fba7406997d5fd1d3c70a699fdc182f437608
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-08-19 11:29:32 +0900

    Add comment

--- lib/jetspider/code_generator.rb
+++ lib/jetspider/code_generator.rb
@@ -226,6 +226,7 @@ module JetSpider
     end
 
     def visit_AddNode(n)
+      # XXX: This approach can't optimize `1 + 2 + 3`
       if n.left.is_a?(RKelly::Nodes::NumberNode) && n.value.is_a?(RKelly::Nodes::NumberNode)
         value =n.left.value + n.value.value
         @asm.int8 value
